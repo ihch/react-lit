4cd4b3740b2109e572e5d19b37f40424d8d8cd56
ihch <nemu0sou@gmail.com>
2021-06-26 20:03:28 +0900

    feat: litのweb componentsをreactで表示した

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { LitStartComponent } from './packages/LitStart.component';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main className="main">
+        <p style={{ margin: 0 }}>Lit Element</p>
+        <div className="lit-start">
+          <LitStartComponent />
+        </div>
+      </main>
     </div>
   );
 }
