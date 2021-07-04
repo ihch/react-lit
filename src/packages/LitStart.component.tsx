abb7b9d9b5786ab8ecb60f85d130f4ed9b5eff04
ihch <nemu0sou@gmail.com>
2021-07-04 22:28:03 +0900

    chore: lit-startのパッケージを使うようにした

--- src/packages/LitStart.component.tsx
+++ src/packages/LitStart.component.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createComponent } from '@lit-labs/react';
-import { LitStart } from './LitStart.js';
+// import { LitStart } from './LitStart.js';
+import { LitStart } from '@ihch/lit-start/LitStart';
 
 export const LitStartComponent = createComponent(
   React,
